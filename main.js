80e5e698ccfd1c527ade2e33624eeb9505d22212
sam9810 <samuelgarces203@gmail.com>
2022-10-11 21:31:05 -0500

    second button interactive functions added

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const hamburguerButtonMenu = document.querySelector('.mobile-menu');
+const hamburguerButton = document.querySelector('.menu');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu () {
     desktopMenu.classList.toggle('inactive')
+}
+
+hamburguerButton.addEventListener('click', toggleMobileMenu);
+
+function toggleMobileMenu () {
+    hamburguerButtonMenu.classList.toggle('inactive-mobile')
 }
