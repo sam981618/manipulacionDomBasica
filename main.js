d341d776fee0d897f9fb20ea84252630349f3192
sam9810 <samuelgarces203@gmail.com>
2022-10-13 21:52:56 -0500

    adding a new button today

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const navEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const hamburguerButtonMenu = document.querySelector('.mobile-menu');
 const hamburguerButton = document.querySelector('.menu');
+const shoppingLogo = document.querySelector('.navbar-shopping-cart');
+const shoppingCart = document.querySelector('.product-detail');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
 
@@ -13,4 +15,10 @@ hamburguerButton.addEventListener('click', toggleMobileMenu);
 
 function toggleMobileMenu () {
     hamburguerButtonMenu.classList.toggle('inactive-mobile')
+}
+
+shoppingLogo.addEventListener('click', toggleShoppingCart);
+
+function toggleShoppingCart () {
+    shoppingCart.classList.toggle('inactive-cart')
 }
