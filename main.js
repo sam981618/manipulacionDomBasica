a36e0f625a5f5971feb61a6eebe01d4dae706131
sam9810 <samuelgarces203@gmail.com>
2022-10-16 19:27:50 -0500

    adding logic to the page

--- main.js
+++ main.js
@@ -8,17 +8,40 @@ const shoppingCart = document.querySelector('.product-detail');
 navEmail.addEventListener('click', toggleDesktopMenu);
 
 function toggleDesktopMenu () {
+    const cartClosed = shoppingCart.classList.contains('inactive');
+
+    if (!cartClosed){
+        //open menu and close cart
+        shoppingCart.classList.add("inactive");
+    }
+
     desktopMenu.classList.toggle('inactive')
 }
 
 hamburguerButton.addEventListener('click', toggleMobileMenu);
 
 function toggleMobileMenu () {
-    hamburguerButtonMenu.classList.toggle('inactive-mobile')
+    const cartClosed = shoppingCart.classList.contains('inactive');
+
+    if (!cartClosed){
+        //open menu and close cart
+        shoppingCart.classList.add("inactive");
+    }
+
+    hamburguerButtonMenu.classList.toggle('inactive')
 }
 
 shoppingLogo.addEventListener('click', toggleShoppingCart);
 
 function toggleShoppingCart () {
-    shoppingCart.classList.toggle('inactive-cart')
+
+    const mobileClosed = hamburguerButtonMenu.classList.contains('inactive');
+
+    if (!mobileClosed){
+        //open cart and close menu
+        hamburguerButtonMenu.classList.add("inactive");
+    }
+
+    shoppingCart.classList.toggle('inactive')
+
 }
